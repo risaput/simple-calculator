ed675812da7c174f5ea6a6722361d5451a220ab7
risaput <arisdevian11@gmail.com>
2023-02-09 06:06:06 +0700

    Second commit

--- script.js
+++ script.js
@@ -1,40 +1,97 @@
 (function() {
 
-  const calculation = document.querySelector('#calc');
-  const result = document.querySelector('#result');
+  const firstInput = document.querySelector('#first');
+  const secondInput = document.querySelector('#second');
   const buttons = document.querySelectorAll('.button');
   
-  buttons.
-    forEach(button => button.addEventListener('click', calculate));
-  
-  let num = '';
-  
-  function calculate() {
-    if (this.dataset.value) {
-      num += this.dataset.value;
-      calculation.value = num;
+  let isCalculate = false;
+
+  buttons.forEach(button => button.addEventListener('click', handleClick))
+    
+  function calculate(value) {
+    return secondInput.value = new Function(`return ${value}`)() || '';
+  }
+
+  function handleClick() {
+    const hasDataSet = this.dataset.value;
+    const hasOperator = this.getAttribute('class').includes('ops')
+    
+    if (hasDataSet) {
+      firstInput.value += hasDataSet;
+      if (hasOperator) {
+        isCalculate = true;
+      }
+      if (isCalculate) {
+        calculate(firstInput.value);
+      }
     }
-    console.log(num)
-   }
+  }
+
+  const clearBtn = document.querySelector('#clear-btn');
+  clearBtn.addEventListener('click', function() {
+    firstInput.value = '';
+    secondInput.value = '';
+  })
+
   
-  // function clear() {
-  //   calculation.value = 0;
-  //   result.value = 0;
-  //   num = ''
-  // }
-  //
-  // function del() {
-  //   num = num > 0 ? num.slice(0, -1) : 0;
-  //   calculation.value = num;
-  // }
-  //
-  // buttons[2].addEventListener('click', del);
-  //
-  // buttons[0].addEventListener('click', clear);
-  //
-  // buttons[19].addEventListener('click', () => {
-  //   result.value = eval(num);
-  //   num = '';
-  // })
+  const delBtn = document.querySelector('#del-btn');
+  delBtn.addEventListener('click', function() {
+    firstInput.value = firstInput.value.slice(0, -1);
+    const id = firstInput.value.length;
+    
+    if (firstInput.value.charAt(id -1) === '+') {
+      calculate(firstInput.value.slice(0, -1))
+    } else {
+      calculate(firstInput.value)
+    }
+
+  })
+  
+  const equalBtn = document.querySelector('#equal-btn');
+  equalBtn.addEventListener('click', function() {
+    alert('click')
+    isCalculate = false;
+  })
 
 }())
+
+
+
+
+// (function() {
+//
+//   const calcu = document.querySelector('#calc');
+//   const result = document.querySelector('#result');
+//   
+//   const buttons = document.querySelectorAll('.button');
+//   buttons.forEach(button => button.addEventListener('click', handleClick));
+//   
+//   function handleClick() {
+//     if (this.dataset.value) {
+//       calculation.value += this.dataset.value;
+//       const foo = new Function(`return ${calculation.value}`)();
+//       console.log(foo)
+//       if (calculation.value.length > 2) {
+//         result.value = eval(calculation.value);
+//       }
+//     }
+//   }
+//   
+//   const clearBtn = document.querySelector('#clear-btn');
+//   clearBtn.addEventListener('click', function() {
+//     calculation.value = '';
+//     result.value = '';
+//   })
+//
+//   const delBtn = document.querySelector('#del-btn');
+//   delBtn.addEventListener('click', function() {
+//     calculation.value = calculation.value.slice(0, -1);
+//     /* result.value = eval(calculation.value); */
+//   })
+//   
+//   const equalBtn = document.querySelector('#equal-btn');
+//   equalBtn.addEventListener('click', function() {
+//     alert('click')
+//   })
+//
+// }())
